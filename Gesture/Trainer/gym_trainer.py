ac45ba8d38b049768eb55bb93923d3215b224dd4
rijulsinghal <rijulsinghal63@gmail.com>
2022-11-01 12:13:43 +0530

    Few changes in gym trainer code.

--- Gesture/Trainer/gym_trainer.py
+++ Gesture/Trainer/gym_trainer.py
@@ -45,23 +45,41 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             landmarks = results.pose_landmarks.landmark
             
             # Get coordinates
-            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            shoulder_left = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            elbow_left = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            wrist_left = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            
+            # Get coordinates
+            shoulder_right = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            elbow_right = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+            wrist_right = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+            
+
+            # Calculate angle
+            angle_left = calculate_angle(shoulder_left, elbow_left, wrist_left)
+            
             
             # Calculate angle
-            angle = calculate_angle(shoulder, elbow, wrist)
+            angle_right = calculate_angle(shoulder_right, elbow_right, wrist_right)
             
             # Visualize angle
-            cv2.putText(image, str(angle), 
-                           tuple(np.multiply(elbow, [640, 480]).astype(int)), 
+            cv2.putText(image, str(angle_left), 
+                           tuple(np.multiply(elbow_left, [640, 480]).astype(int)), 
                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
                                 )
             
+            # Visualize angle
+            cv2.putText(image, str(angle_right), 
+                           tuple(np.multiply(elbow_right, [440, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                )
+            
+            
             # Curl counter logic
-            if angle > 160:
+
+            if angle_left > 160 and angle_right > 160:
                 stage = "down"
-            if angle < 30 and stage =='down':
+            if angle_left < 30 and angle_left < 30 and stage =='down':
                 stage="up"
                 counter +=1
                 print(counter)
