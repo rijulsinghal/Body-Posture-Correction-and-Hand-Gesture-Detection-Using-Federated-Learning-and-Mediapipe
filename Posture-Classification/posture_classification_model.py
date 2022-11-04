1fb9ef6318c709412af3d423ad816d7eaf460651
rijulsinghal <rijulsinghal63@gmail.com>
2022-11-04 22:09:06 +0530

    Added Posture Classification Code.

---
+++ Posture-Classification/posture_classification_model.py
@@ -0,0 +1,114 @@
+import os
+import cv2
+import pickle
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+import keras
+import tensorflow
+import pickle
+from tensorflow.keras.models import Model
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.applications import VGG19
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Input, Lambda, Dense, Flatten, Dropout, BatchNormalization, Activation
+
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, recall_score, precision_score, f1_score 
+
+dataset_path = os.listdir("C:/Users/HP/Documents/Capstone-Project/Implementation/Dataset/Yoga-Dataset/DATASET/TRAIN")
+labels = os.listdir("C:/Users/HP/Documents/Capstone-Project/Implementation/Dataset/Yoga-Dataset/DATASET/TRAIN")
+print (labels)  
+
+img_size = 224
+def get_data(data_dir):
+    data = [] 
+    for label in labels: 
+        path = os.path.join(data_dir, label)
+        class_num = labels.index(label)
+        for img in os.listdir(path):
+            try:
+                img_arr = cv2.imread(os.path.join(path, img))[...,::-1] #convert BGR to RGB format
+                resized_arr = cv2.resize(img_arr, (img_size, img_size)) # Reshaping images to preferred size
+                data.append([resized_arr, class_num])
+            except Exception as e:
+                print(e)
+    return np.array(data)
+
+train = get_data("C:/Users/HP/Documents/Capstone-Project/Implementation/Dataset/Yoga-Dataset/DATASET/TRAIN")
+val = get_data("C:/Users/HP/Documents/Capstone-Project/Implementation/Dataset/Yoga-Dataset/DATASET/TEST")
+
+x_train = []
+y_train = []
+x_val = []
+y_val = []
+
+for feature, label in train:
+  x_train.append(feature)
+  y_train.append(label)
+
+for feature, label in val:
+  x_val.append(feature)
+  y_val.append(label)
+
+# Normalize the data
+x_train = np.array(x_train) / 255
+x_val = np.array(x_val) / 255
+
+x_train.reshape(-1, img_size, img_size, 1)
+y_train = np.array(y_train)
+
+x_val.reshape(-1, img_size, img_size, 1)
+y_val = np.array(y_val)
+
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range = 30,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.2, # Randomly zoom image 
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip = True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+
+datagen.fit(x_train)
+
+vgg = VGG19(input_shape = (224,224,3), weights = 'imagenet', include_top = False)
+
+for layer in vgg.layers:
+    layer.trainable = False
+
+x = Flatten()(vgg.output)
+prediction = Dense(len(labels), activation='softmax')(x)
+
+model = Model(inputs=vgg.input, outputs=prediction)
+model.summary()
+model.compile(
+  loss='sparse_categorical_crossentropy',
+  optimizer="adam",
+  metrics=['accuracy']
+)
+
+plot_model(model=model, show_shapes=True)
+
+early_stop = EarlyStopping(monitor = 'val_loss', mode='min', verbose = 1, patience = 5)
+
+history = model.fit(x_train, y_train, validation_data = (x_val, y_val), epochs = 3, callbacks=[early_stop], batch_size = 30,
+                    shuffle=True)
+
+
+model.save("models/posture-classification")
+reconstruct_model = keras.models.load_model("models/posture-classification")
+result = reconstruct_model.predict(x_val)
+
+y_pred=np.argmax(result,axis=1)
+print(classification_report(y_pred,y_val))
+
